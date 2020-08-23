56985bb562caee099214431a9298d7a34725a1cf
snh1003 <lunasaprismriver57@gmail.com>
2020-08-23 09:43:27 +0900

    add button component

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,12 @@ import GlobalStyle from "./GlobalStyle";
 
 ReactDOM.render(
     <>
-        <GlobalStyle />
+        <GlobalStyle/>
         <React.StrictMode>
-            <App />
+            <App/>
         </React.StrictMode>
     </>,
-  document.getElementById('root')
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
